35f1dcac671804d9a3a4e0ec04d2f2f5f7b37183
napbit <nap.tyo@gmail.com>
2020-02-07 10:13:53 +0700

    create checks for and create method to read file

--- src/main/java/id/napbit/Main.java
+++ src/main/java/id/napbit/Main.java
@@ -1,9 +1,73 @@
 package id.napbit;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import id.napbit.model.Parameter;
+
+public class Main {
+	
+	public static final int NO_FILE_PATH = 40;
+	public static final int INVALID_FILE_PATH = 41;
+	public static final int INTERNAL_ERROR = 50;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		if (args.length <= 0) {
+			System.out.println("No file path arguments passed.");
+			System.exit(NO_FILE_PATH);
+		}
+		
+		String pathToFileArg = args[0];
+		
+		try {
+			if (!new File(pathToFileArg).exists()) {
+				System.out.println("Invalid path as no file is found.");
+				System.exit(INVALID_FILE_PATH);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(INTERNAL_ERROR);
+		}
+		
+		readFile(pathToFileArg);
+	}
+	
+	private static void readFile(String pathToFile) {
+		List<Parameter> parameterList = new ArrayList<Parameter>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
+		    String line;
+		    
+		    while ((line = br.readLine()) != null) {
+		    	String[] parameter = line.split(" ");
+		    	
+		    	if (parameter.length > 2) {
+		    		parameterList.add(
+		    				new Parameter()
+		    				.withParameterName(parameter[0].trim())
+		    				.withParameterDataType(parameter[1].trim())
+		    				.withParameterType("OUT"));
+		    	} else {
+		    		parameterList.add(
+		    				new Parameter()
+		    				.withParameterName(parameter[0].trim())
+		    				.withParameterDataType(parameter[1].trim())
+		    				.withParameterType("IN"));
+		    	}
+		    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for (Parameter parameter : parameterList) {
+			System.out.println(parameter.getParameterName() + " " + parameter.getParameterDataType() + " " + parameter.getParameterType());
+		}
+		
+		System.out.println("Total Size: " + parameterList.size());
 	}
 
 }
